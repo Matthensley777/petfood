83515a5a467f9d81b4d327c66cc7615ec6265da2
Matt Hensley <matt.hensley777@gmail.com>
2017-03-11 09:32:02 -0600

    added catfood, trying to get all of them to work together

--- food.js
+++ food.js
@@ -1,6 +1,7 @@
 console.log("First line in js file: ", Date.now());
 
 var petFood = document.getElementById("petFood");
+var catFood = document.getElementById("catFood");
 
 function makeDom(dogData) {
     var dogFood = "";
@@ -29,6 +30,34 @@ function makeDom(dogData) {
 }
 
 
+
+function makeDom(catData) {
+    var catFood = "";
+
+
+    for (var i = 0; i < catData.catBrands.length; i++) {
+        var currentCatBrand = catData.catBrands[i];
+        catFood += `<div class="col-sm-6 col-md-4">`;
+        catFood += `<h3>${currentCatBrand.name}</h3>`;
+        catFood += `<div class="caption">`;
+
+        for (var j = 0; j < currentCatBrand.types.length; j++) {
+            var currentType = currentCatBrand.types[j];
+            catFood += `<p>${currentType.type}</p>`;
+
+            for (var k = 0; k < currentType.volumes.length; k++) {
+                var currentVolume = currentType.volumes[k];
+                catFood += `<p>$${currentVolume.price} ${currentVolume.name}</p>`;
+
+            }
+
+        }
+        catFood += `</div></div>`;
+    }
+    catFood.innerHTML = catFood;
+}
+
+
 function executeThisCodeAfterFileLoaded() {
 
     var data = JSON.parse(this.responseText);
@@ -41,9 +70,8 @@ function executeThisCodeAfterFileLoaded() {
 var myRequest = new XMLHttpRequest();
 myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
 myRequest.open("GET", "main.json");
-// myRequest.open("GET", "catfood.json");
+myRequest.open("GET", "catfood.json");
 
 myRequest.send();
 
 
-// console.log("last line in js file: ", Date.now());
